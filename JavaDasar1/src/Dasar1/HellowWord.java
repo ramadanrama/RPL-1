0b9e77e525e9f55156566ca9914a3050cc9a879e
unknown <ramadanramadhan133@gmail.com>
2022-10-18 08:31:02 +0700

    Day 01

---
+++ JavaDasar1/src/Dasar1/HellowWord.java
@@ -0,0 +1,11 @@
+package Dasar1;
+ 
+public class HellowWord { 
+    public static void main(String[] args) {
+        System.out.println("hay hellow word"); 
+        System.out.println("selamat datang"); 
+        System.out.println("selamat jalan"); 
+        System.out.println("selamat anda menang"); 
+    }
+    
+}
---
+++ JavaDasar1/src/Dasar1/dataDiri1.java
@@ -0,0 +1,14 @@
+package Dasar1;
+public class dataDiri1 {
+    public static void main(String[] args) {
+        
+        String nama = "ramadhan"; 
+        int umur = 18; 
+        boolean status = (false); 
+        
+        System.out.println("nama saya :" +nama); 
+        System.out.println("umur :" +umur);
+        System.out.println("status mahasiswa :"+status); 
+        
+    }
+}
