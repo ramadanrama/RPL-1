ac895aab1212cf2be59dbe636588aa0d7f002b3e
unknown <ramadanramadhan133@gmail.com>
2022-10-19 17:36:31 +0700

    pengurangan

---
+++ operatorPengurangan.java/src/operatorpengurangan/java/OperatorPenguranganJava.java
@@ -0,0 +1,25 @@
+
+ 
+package operatorpengurangan.java;
+
+import java.util.Scanner;
+
+public class OperatorPenguranganJava {
+    public static void main(String[] args) {
+    int angka1 ;  
+    int angka2; 
+    int hasil; 
+    
+    Scanner input= new Scanner(System.in); 
+    
+        System.out.println("masukkan angkapertama:" ); 
+        angka1=input.nextInt(); 
+        System.out.println("masukkan angka kedua :" ); 
+        angka2=input.nextInt(); 
+        System.out.println( ); 
+        hasil = input.nextInt();
+
+     
+    }
+    
+}
---
+++ pengurangan/src/pengurangan/java.java
@@ -0,0 +1,10 @@
+package pengurangan;
+public class java {
+    public static void main(String[] args) {
+         int angka1 = 10; 
+         int angka2 = 5; 
+         int hasil = angka1 - angka2; 
+         System.out.println("hasilnya adalah :"+hasil);
+    }
+    
+}
