286430e5fe5260400e3d159594a9cbe8edd4dbe2
unknown <ramadanramadhan133@gmail.com>
2022-10-19 11:01:52 +0700

    rpl

---
+++ rpl project/src/penjumlahan.java
@@ -0,0 +1,10 @@
+
+public class penjumlahan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        int hasil=b+a;
+        System.out.println(hasil);
+    }
+    
+}
