7e76106722f52f3e3a8c457b4cec41d6a95e89cb
unknown <ramadanramadhan133@gmail.com>
2022-10-21 01:35:43 +0700

    pembagian

---
+++ pembagian.java/src/pembagian/java/PembagianJava.java
@@ -0,0 +1,14 @@
+
+package pembagian.java;
+
+
+public class PembagianJava {
+
+    public static void main(String[] args) {
+        int a=28;
+        int b=6;
+        int hasil=a/b;
+        System.out.println(hasil);
+    }
+    
+}
