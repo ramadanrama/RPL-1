485006ad254d78db20ac846a100056ce858b031b
unknown <ramadanramadhan133@gmail.com>
2022-10-21 23:09:37 +0700

    perkalian

---
+++ perkalian/src/NewClass.java
@@ -0,0 +1,10 @@
+
+public class NewClass {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 7;
+        int hasil = a*b;
+        System.out.println(hasil);
+    }
+    
+}
