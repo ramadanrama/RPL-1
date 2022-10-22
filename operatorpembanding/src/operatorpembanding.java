bf557dfaa7f21e41df2f973cdda9ad85f2c865fe
unknown <ramadanramadhan133@gmail.com>
2022-10-22 13:31:55 +0700

    operatorpembanding

---
+++ operatorpembanding/src/operatorpembanding.java
@@ -0,0 +1,14 @@
+
+
+public class operatorpembanding {
+    public static void main(String[] args) {
+        int nilai_A= 14;
+        int nilai_B= 16;
+        boolean hasil;
+        //apakah a lebih kecil besar dari b?
+        hasil = nilai_A < nilai_B;
+        System.out.println(hasil);
+        
+    }
+    
+}
