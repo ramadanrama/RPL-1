6c3bbcbd04fa657a91d8756fe7ad21b437537f14
unknown <ramadanramadhan133@gmail.com>
2022-10-24 14:58:43 +0700

    luas_lingkaran

---
+++ dataramagaji/src/pertemuan8.java
@@ -0,0 +1,20 @@
+
+public class pertemuan8 {
+    public static void main(String[] args) {
+        
+        //deklarasi
+        double pi, r, luas;
+        
+        //deksripsi
+        r = 10.0;
+        pi = 3.14;
+        
+        //proses
+        luas = pi * r * r;
+        
+        //output
+        System.out.println("luas lingkaran = ");
+        System.out.println(luas);
+    }
+    
+}
