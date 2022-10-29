e345d1685ecd3f3efb2e556609120a2521f8d3ce
unknown <ramadanramadhan133@gmail.com>
2022-10-29 07:07:42 +0700

    pertemuan12

---
+++ dataramagaji/src/pertemuan12/projek12.java
@@ -0,0 +1,30 @@
+
+package pertemuan12;
+
+import java.util.Scanner;
+
+public class projek12 {
+    public static void main(String[] args) {
+        
+    
+        String namadepan;
+        String namabelakang;
+        String notelp;
+        float  tinggi;
+        double beratbadan;
+        int    umur;
+        boolean statusnikah;
+        
+        Scanner Scan = new Scanner("System.in");
+        
+        System.out.println("namadepan    =ramadhan");
+        System.out.println("namabelakang =rama");
+        System.out.println("notelp       =081357367303");
+        System.out.println("beratbadan   =35kg");
+        System.out.println("umur         =18");
+        System.out.println("statusnikah  =false");
+     }
+} 
+     
+    
+
