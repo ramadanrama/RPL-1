abc590d12e0ed1ba82510c199ab866f7082abc6f
unknown <ramadanramadhan133@gmail.com>
2022-11-01 10:31:14 +0700

    pertemuan 15

--- dataramagaji/src/projeck14/rama.java
+++ dataramagaji/src/projeck14/rama.java
@@ -4,13 +4,13 @@ package projeck14;
 import java.util.Scanner;
 public class rama {
     public static void main(String[] args) {
-        // creates an object of Scanner
+        // objek
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter your name: ");
+    System.out.print("siapa akau : ");
 
-    // reads the entire line
-    String value = input.nextLine();
-    System.out.println("Using nextLine(): " + value);
+    // baris
+    String rama = input.nextLine();
+    System.out.println("rama : " + rama);
 
     input.close();
   }
---
+++ dataramagaji/src/projeckrama/pertemuan15.java
@@ -0,0 +1,21 @@
+
+package projeckrama;
+
+import java.util.Scanner;
+
+
+public class  pertemuan15 {
+    public static void main(String[] args) {
+
+    // membuat objek pemindai
+    Scanner input  = new  Scanner  (System.in);
+    System.out.print(" nama anak rantau : ");
+
+    // membaca nilai ganda
+    double rama = input.nextDouble();
+    System.out.println("rama :" + rama);
+
+    input.close();
+  }
+}
+
