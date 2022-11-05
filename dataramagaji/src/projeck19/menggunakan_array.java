ebdabc26c803a8466fb7a102405b910aec6b0f73
unknown <ramadanramadhan133@gmail.com>
2022-11-05 09:41:06 +0700

    19

---
+++ dataramagaji/src/projeck19/menggunakan_array.java
@@ -0,0 +1,19 @@
+
+package projeck19;
+
+
+public class menggunakan_array {
+    public static void main(String[] args) {
+  
+   // nama-nama outputnya
+   int[] String = {200, 250, 200, 20, 5};
+
+   // mengakses setiap angka 
+   System.out.println("Elements Array:");
+   System.out.println("pertama  : " + String[0]);
+   System.out.println("ke  dua  : " + String[1]);
+   System.out.println("ke tiga  : " + String[2]);
+   System.out.println("ke empat : " + String[3]);
+   System.out.println("ke lima  : " + String[4]);
+ }
+}
