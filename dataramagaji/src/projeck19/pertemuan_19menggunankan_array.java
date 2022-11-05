ae2d22f3ece169fc7469ffe41e7fb68cbf63fc62
unknown <ramadanramadhan133@gmail.com>
2022-11-05 12:04:06 +0700

    19

---
+++ dataramagaji/src/projeck19/pertemuan_19menggunankan_array.java
@@ -0,0 +1,18 @@
+
+package projeck19;
+
+
+public class pertemuan_19menggunankan_array {
+     public static void main(String[] args) {
+  
+   // nama-nama outputnya
+   String[] nama = {"rama, arif, alim, rey, jon"};
+
+   // mengakses nama nama 
+   System.out.println(" Array:");
+         for (int i = 0; i < nama.length; i++) {
+             System.out.println(nama[i]);
+         }
+ 
+ }
+}
