24350ef2c116602f00d3eab34ebb7677058f2457
unknown <ramadanramadhan133@gmail.com>
2022-11-06 09:23:30 +0700

    rpl day 20

---
+++ BelajarArrayDasar/src/belajararraydasar/BelajarArrayDasar.java
@@ -0,0 +1,16 @@
+ 
+package belajararraydasar;
+
+public class BelajarArrayDasar {
+    public static void main(String[] args) {
+        String[] nama = {"andi","budi","laila","rama"}; 
+        
+        
+        //menampilkan nama pada variabel 
+        System.out.println(nama[0]); 
+        System.out.println(nama[1]); 
+        System.out.println(nama[2]);
+        System.out.println(nama[3]);  
+    }
+    
+}
