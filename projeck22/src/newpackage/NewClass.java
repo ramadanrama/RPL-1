839e90b41d998197bc3807df1bc1d2343f32711a
unknown <ramadanramadhan133@gmail.com>
2022-11-09 09:34:03 +0700

    23

---
+++ projeck22/src/newpackage/NewClass.java
@@ -0,0 +1,12 @@
+
+package newpackage;
+
+
+public class NewClass {
+    public static void main(String[] args) {
+        
+        //menampilkan hello word
+        System.out.println("hello word");
+        
+    }
+}
