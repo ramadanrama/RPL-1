f6515bc07a18cf9670e21dfff7baedb99f9d8c40
unknown <ramadanramadhan133@gmail.com>
2022-11-09 09:40:57 +0700

    23

---
+++ projeck23/src/helloword/pw.java
@@ -0,0 +1,15 @@
+
+package helloword;
+
+
+public class pw {
+     
+    public static void main(String[] args) {
+        System.out.println("hay hellow word"); 
+        System.out.println("selamat datang"); 
+        System.out.println("selamat jalan"); 
+        System.out.println("selamat anda menang"); 
+    }
+    
+
+}
