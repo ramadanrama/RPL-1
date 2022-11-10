f8c9dcea5ddbc7d06dcac5248893c5b7d867cbce
unknown <ramadanramadhan133@gmail.com>
2022-11-10 13:08:49 +0700

    25

---
+++ JAVA/src/Project25/main.java
@@ -0,0 +1,19 @@
+
+package Project25;
+
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        
+        Scanner r = new Scanner(System.in);
+        String nama;
+        
+        //masukkan nama pengguna dan tekan enter
+        System.out.println("nama saya adalah "); 
+    nama = r.nextLine();   
+       
+    System.out.println("nama: " + nama);
+    
+        
+    }
+}
