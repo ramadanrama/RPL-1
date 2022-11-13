1c9e3f79344addd3a202b99519d8865e5ef60b17
unknown <ramadanramadhan133@gmail.com>
2022-11-13 08:14:13 +0700

    27

---
+++ JAVA/src/projeck27/solution.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeck27;
+
+/**
+ *
+ * @author My ASUS
+ */
+public class solution {
+    
+}
