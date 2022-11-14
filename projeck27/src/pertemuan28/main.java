3b9818032512257a2438a510081dc77a65739003
unknown <ramadanramadhan133@gmail.com>
2022-11-14 09:26:58 +0700

    28

---
+++ projeck27/src/pertemuan28/main.java
@@ -0,0 +1,17 @@
+
+package pertemuan28;
+
+
+public class main {
+    public static void main(String[] args) {
+        
+        int[] ArrayAngka ={2,3,4,5,6,7,8,9,};
+        
+       // looping standard");
+        System.out.println("looping standard");
+        for(int i = 0; i<10; i++){
+            System.out.println("index ke-"+i+" adalah = "+ ArrayAngka[i]); 
+            
+       }
+    }
+}
