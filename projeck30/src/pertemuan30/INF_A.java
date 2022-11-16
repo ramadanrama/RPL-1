4af35c59be9d164ff6b1cdb65b596ea46e9cf72f
unknown <ramadanramadhan133@gmail.com>
2022-11-16 09:41:22 +0700

    30

---
+++ projeck30/src/pertemuan30/INF_A.java
@@ -0,0 +1,26 @@
+
+package pertemuan30;
+
+import java.util.Scanner;
+
+public class INF_A {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        String nama,jurusan;
+        int nilai;
+        
+        System.out.println("rama");
+        nama = in.nextLine();
+        
+        System.out.println("INFORMATIKA A");
+        jurusan = in.nextLine();
+        
+        System.out.println("10");
+        nilai = in.nextInt();
+        
+      }
+    }
+    
+    
+
