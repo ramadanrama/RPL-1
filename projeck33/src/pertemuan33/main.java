fb3781f9b2d1fd107145e70612081540a972cf03
unknown <ramadanramadhan133@gmail.com>
2022-11-19 09:23:51 +0700

    33

---
+++ projeck33/src/pertemuan33/main.java
@@ -0,0 +1,20 @@
+
+package pertemuan33;
+
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+      
+        int [] numbers = {1, 2, 3, 4, 5, 6};
+        int [] positiveNumbers = numbers;   
+      
+        
+        numbers[0] = -1;
+
+        
+        for (int number: positiveNumbers) {
+            System.out.print(number + " , ");
+        }
+    }
+}
