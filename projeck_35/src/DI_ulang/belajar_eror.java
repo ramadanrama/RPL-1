8376d76b241c64bcfb577445d9d19544883b9c5c
unknown <ramadanramadhan133@gmail.com>
2022-11-21 12:21:41 +0700

    35

---
+++ projeck_35/src/DI_ulang/belajar_eror.java
@@ -0,0 +1,17 @@
+
+package DI_ulang;
+
+import java.util.Scanner;
+public class belajar_eror {
+    public static void main(String[] args) {
+        int nilai;
+        Scanner Sc = new Scanner (System.in);
+        
+        System.out.println("tampilan eror");
+        {
+            System.out.println("salah");
+        }
+    nilai=100;
+    }
+}
+  
